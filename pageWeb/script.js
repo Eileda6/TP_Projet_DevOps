471331348788167bff988deec7f54603bcb66d30
Eileda6 <145599771+Eileda6@users.noreply.github.com>
2025-04-04 16:21:41 +0200

    Ajout du dossier pageWeb

---
+++ pageWeb/script.js
@@ -0,0 +1,97 @@
+const symbols = ['🍎','🍌','🍇','🍓','🍉','🍒','🍍','🥝'];
+let cards = [...symbols, ...symbols]; // 2x chaque symbole
+let revealedCards = [];
+let lockBoard = false;
+let score = 0;
+
+
+shuffle(cards);
+
+const gameBoard = document.getElementById("gameBoard");
+
+cards.forEach((symbol, index) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.dataset.symbol = symbol;
+  card.dataset.index = index;
+  card.addEventListener("click", handleCardClick);
+  gameBoard.appendChild(card);
+});
+
+function handleCardClick(e) {
+  const card = e.target;
+  if (lockBoard || card.classList.contains("revealed")) return;
+
+  revealCard(card);
+  revealedCards.push(card);
+
+  if (revealedCards.length === 2) {
+
+    score++;
+    document.getElementById("score").textContent = score;
+    lockBoard = true;
+    const [card1, card2] = revealedCards;
+    if (card1.dataset.symbol === card2.dataset.symbol) {
+      revealedCards = [];
+      lockBoard = false;
+      if (document.querySelectorAll('.card.revealed').length === cards.length) {
+        setTimeout(() => {
+          document.getElementById("finalScore").textContent = score;
+          document.getElementById("winScreen").classList.remove("hidden");
+        }, 500);
+      }
+    } else {
+      setTimeout(() => {
+        hideCard(card1);
+        hideCard(card2);
+        revealedCards = [];
+        lockBoard = false;
+      }, 1000);
+    }
+  }
+  if (document.querySelectorAll('.card.revealed').length === cards.length) {
+    setTimeout(() => {
+      document.getElementById("winScreen").classList.remove("hidden");
+    }, 500);
+  }
+  
+}
+
+function revealCard(card) {
+  card.classList.add("revealed");
+  card.textContent = card.dataset.symbol;
+}
+
+function hideCard(card) {
+  card.classList.remove("revealed");
+  card.textContent = '';
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function restartGame() {
+    // Réinitialise l'écran
+    document.getElementById("winScreen").classList.add("hidden");
+    gameBoard.innerHTML = "";
+    revealedCards = [];
+    lockBoard = false;
+    score = 0;
+    document.getElementById("score").textContent = score;
+    // Re-mélange et recrée les cartes
+    shuffle(cards);
+    cards.forEach((symbol, index) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.dataset.symbol = symbol;
+      card.dataset.index = index;
+      card.addEventListener("click", handleCardClick);
+      gameBoard.appendChild(card);
+    });
+    
+  }
+  
